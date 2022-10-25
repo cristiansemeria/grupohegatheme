edbae69ff987f8321149319dacae63f512ad065a
cristiansemeria <cristiansemeria@gmail.com>
2022-10-25 13:31:43 -0500

    24/10/2022

--- database/conection.php
+++
@@ -1,8 +0,0 @@
-<?php
-global $wpdb;
-$db = $wpdb->prefix. 'equipos';
-$equipo_id = get_the_id();
-$marca = $wpdb->get_var("SELECT `marca` FROM $db WHERE `equipo_ID` = $equipo_id");
-$modelo = $wpdb->get_var("SELECT `modelo` FROM $db WHERE `equipo_ID` = $equipo_id");
-$informacion_equipo = $wpdb->get_var("SELECT `informacion` FROM $db WHERE `equipo_ID` = $equipo_id");
-?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Hega
+ */
+
+get_header();
+?>
+
+<div class="content">
+
+</div>
+
+<?php
+get_footer();
+?>
+
+ 
